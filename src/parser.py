1359b6e2f9f86cbd14b5e1b19bba06616b77b6ed
Paulina Szubarczyk <paulinaszubarczyk@gmail.com>
2015-01-10 18:55:05 +0100

    Fixed

--- src/parser.py
+++ src/parser.py
@@ -10,43 +10,45 @@ def parse_document(document):
     paragraph_end = len(document) - 1
     state = 1
 
-    for i in range(len(document)):
+    i = 0
+    while i < len(document):
         ch = document[i]
+
         if ch == '\n':
             end = True
 
             while i+1 < (len(document) - 1) and document[i+1] == '\r':
                 i = i + 1
-                print 'Skiping ' + repr(document[i])
+                print 'Skiping ' + repr(document[i]) + " " + str(i)
+
 
         if state == 1:
             if ~ch.isspace():
                 state = 2
                 paragraph_begin = i
-            continue
 
-        if state == 2:
+
+        elif state == 2:
             if end:
                 end = False
                 paragraph_end = i
                 state = 3
-            continue
 
-        if state == 3:
-            print 'State ' + str(state) + ' end ' + str(end)
+
+        elif state == 3:
             if end:
                 state = 1
                 end = False
                 l.paragraph(paragraph_begin, paragraph_end)
             else:
                 state = 2
-            continue
+        i = i + 1
 
     paragraph_end = len(document) - 1
 
     if state == 2 or state == 3:
         l.paragraph(paragraph_begin, paragraph_end)
 
-    # l.print_text()
-    # l.print_labeled()
+    l.print_text()
+    l.print_labeled()
     l.write_to_file()
